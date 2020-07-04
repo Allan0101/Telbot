4d05f8283d9f8d6d0265917d7c435a2af613c9b6
Allan0101 <67823257+Allan0101@users.noreply.github.com>
2020-07-04 13:08:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 bot = telebot.TeleBot('919261879:AAH7Vto0oADnmCr6ljE85T5e4WOxPscwqtM')
 
 @bot.message_handler(content_types=['text'])
-def lalala(message):
+def Send(message):
 	bot.send_message(message.chat.id, message.text)
 
 bot.polling(none_stop=True)
